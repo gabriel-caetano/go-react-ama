9415e080800d4cef2f84ce2911a24edc1401dd2a
Gabriel Caetano <gabrielvscaetano@gmail.com>
2024-08-07 11:31:08 -0300

    add middlewares for the router

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gabriel-caetano/go-react-ama/internal/store/pgstore"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type apiHandler struct {
@@ -23,6 +24,7 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 	}
 
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
 	a.r = r
 	return a
 }
