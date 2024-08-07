04a428160109f36e7e1569823376a8c485070c10
Gabriel Caetano <gabrielvscaetano@gmail.com>
2024-08-07 11:28:52 -0300

    init: day one of rocketseat ask me anything project

---
+++ server/cmd/go-react/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/gabriel-caetano/go-react-ama/internal/api"
+	"github.com/gabriel-caetano/go-react-ama/internal/store/pgstore"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	port := os.Getenv("DATABASE_PORT")
+	dbname := os.Getenv("DATABASE_NAME")
+
+	connString := "user=" + user + " password=" + password + " port=" + port + " dbname=" + dbname
+
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}
